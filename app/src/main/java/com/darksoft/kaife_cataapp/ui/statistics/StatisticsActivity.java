db6b266572d491af1d7835594109cd42d650df83
Juanchis <juan.moonteroo@gmail.com>
2022-06-09 03:42:05 -0400

    App terminada 100%

--- app/src/main/java/com/darksoft/kaife_cataapp/ui/statistics/StatisticsActivity.java
+++ app/src/main/java/com/darksoft/kaife_cataapp/ui/statistics/StatisticsActivity.java
@@ -7,12 +7,20 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import com.darksoft.kaife_cataapp.databinding.ActivityStatisticsBinding;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -23,80 +31,89 @@ public class StatisticsActivity extends AppCompatActivity {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    private ArrayList<BarEntry> femenino = new ArrayList<>();
-    private ArrayList<BarEntry> masculino = new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityStatisticsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        SharedPreferences preferences = getSharedPreferences("generos", MODE_PRIVATE);
-        String female = preferences.getString("femenino", "NO");
-
         binding.back.setOnClickListener(v ->{
-            System.out.println(female);
             finish();
         });
 
-        getData();
         loadData();
     }
 
-    private void loadData() {
-        BarDataSet barDataSet1 = new BarDataSet(femenino, "Femenino");
-        barDataSet1.setColors(ColorTemplate.COLORFUL_COLORS);
-        barDataSet1.setValueTextColor(Color.BLACK);
-        barDataSet1.setValueTextSize(16f);
+    private void loadData(){
+        //SetupPieChart
+        binding.pieChart1.setDrawHoleEnabled(true);
+        binding.pieChart1.setUsePercentValues(true);
+        binding.pieChart1.setEntryLabelTextSize(12);
+        binding.pieChart1.setEntryLabelColor(Color.BLACK);
+        binding.pieChart1.setCenterText("Género");
+        binding.pieChart1.setCenterTextSize(24);
+        binding.pieChart1.getDescription().setEnabled(false);
 
-        BarDataSet barDataSet2 = new BarDataSet(masculino, "Masculino");
-        barDataSet2.setColor(Color.BLUE);
-        barDataSet2.setValueTextColor(Color.BLACK);
-        barDataSet2.setValueTextSize(16f);
+        Legend legend = binding.pieChart1.getLegend();
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        legend.setOrientation(Legend.LegendOrientation.VERTICAL);
+        legend.setDrawInside(false);
+        legend.setEnabled(true);
 
-        BarData barData = new BarData(barDataSet1, barDataSet2);
-        binding.chart1.setData(barData);
-        binding.chart1.getDescription().setEnabled(false);
-    }
+        ArrayList<String> datos = new ArrayList<>();
 
-    private void getData(){
-
-        SharedPreferences preferences = getSharedPreferences("genero", MODE_PRIVATE);
-        ArrayList<String> global = new ArrayList<>();
-
-        db.collection("Respuestas").get().addOnSuccessListener(task -> {
-
-            for (DocumentSnapshot document: task){
+        //GetData
+        db.collection("Respuestas").get().addOnSuccessListener(queryDocumentSnapshots -> {
+            for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
                 String valor = document.getString("genero");
-                global.add(valor);
+                datos.add(valor);
             }
 
-            int male = 0, female = 0;
-            for (int i = 0; i < global.size(); i++){
-                if (Objects.equals(global.get(i), "femenino"))
+
+            int female = 0, male = 0;
+            for(int i = 0; i < datos.size(); i++){
+                if (Objects.equals(datos.get(i), "femenino"))
                     female++;
-                if (Objects.equals(global.get(i), "masculino"))
+                if (Objects.equals(datos.get(i), "masculino"))
                     male++;
             }
 
-//            femenino.add(new BarEntry(2, female));
-//            masculino.add(new BarEntry(4, male));
+            binding.tAllAnswersFemenino.setText(String.valueOf(female));
+            binding.tAllAnswersMasculino.setText(String.valueOf(male));
 
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putInt("femenino", female);
-            editor.putInt("masculino", male);
-            editor.commit();
+            //loadPieChartData
+            ArrayList<PieEntry> entries = new ArrayList<>();
+            entries.add(new PieEntry(female, "Femenino"));
+            entries.add(new PieEntry(male, "Masculino"));
+
+            ArrayList<Integer> colors = new ArrayList<>();
+            for (int color : ColorTemplate.MATERIAL_COLORS) {
+                colors.add(color);
+            }
+
+            for (int color : ColorTemplate.PASTEL_COLORS) {
+                colors.add(color);
+            }
+
+            PieDataSet dataSet = new PieDataSet(entries, "Respuestas");
+            dataSet.setColors(colors);
+
+            PieData data = new PieData(dataSet);
+            data.setDrawValues(true);
+            data.setValueFormatter(new PercentFormatter(binding.pieChart1));
+            data.setValueTextSize(12f);
+            data.setValueTextColor(Color.BLACK);
+
+            binding.pieChart1.setData(data);
+            binding.pieChart1.invalidate();
+            binding.pieChart1.animateY(1400, Easing.EaseInOutQuad);
 
         });
 
-        int female = preferences.getInt("femenino", 0);
-        int male = preferences.getInt("masculino", 0);
-
-        femenino.add(new BarEntry(2, female));
-        masculino.add(new BarEntry(4, male));
 
     }
+
 
     @Override
     public void onBackPressed() {
